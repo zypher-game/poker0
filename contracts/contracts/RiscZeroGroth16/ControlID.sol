039c645e17c3092de7deb019286d383f4d9e7812
confuseSUN <778398626@qq.com>
2024-02-22 16:45:10 +0800

    add contracts

---
+++ contracts/contracts/RiscZeroGroth16/ControlID.sol
@@ -0,0 +1,8 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+library ControlID {
+    uint256 public constant CONTROL_ID_0 = 0x447d7e12291364db4bc5421164880129;
+    uint256 public constant CONTROL_ID_1 = 0x12c49ad247d28a32147e13615c6c81f9;
+}
---
+++ contracts/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      mining: {
+        auto: true,
+        interval: 2000
+      },
+    },
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: './build/cache',
+    artifacts: './build/artifacts',
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  mocha: {
+    timeout: 200000
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: true,
+  },
+};
